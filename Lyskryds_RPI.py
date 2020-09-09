ced5ebcd3883be134f6cddec17354147297da807
ThorMikkelsen <70999980+ThorMikkelsen@users.noreply.github.com>
2020-09-09 11:25:52 +0200

    Add files via upload

--- Lyskryds_RPI.py
+++ Lyskryds_RPI.py
@@ -45,17 +45,50 @@ def redred(x):# Udgangs punkt for lyskrydset
         EVred.on()
         sleep(2)
         return NS(x)
-def NS():
+def NS(x):
+    NSred.on()
+    NSgul.on()
+    EVred.on()
+    sleep(2)
+    return NS_green(x)
 
-def NS_green():
+def NS_green(x):
+    NSgreen.on()
+    EVred.on()
+    NSgul.off()
+    NSred.off
+    sleep(2)
+    return NS_gul(x)
 
-def NS_gul():
+def NS_gul(x):
+    NSgul.on()
+    EVred.on()
+    NSgreen.off()
+    Sleep(2)
+    return redred(x)
 
-def EV():
+def EV(x):
+    EVred.on()
+    EVgul.on()
+    NSred.on()
+    sleep(2)
+    return EV_green(x)
 
-def EV_green():
+def EV_green(x):
+    EVgreen.on()
+    NSred.on()
+    EVgul.off()
+    EVred.off
+    sleep(2)
+    return EV_gul(x)
 
-def EV_gul():
+def EV_gul(x):
+    EVgul.on()
+    NSred.on()
+    EVgreen.off()
+    Sleep(2)
+    return redred(x)
+
 
 state=redred(x="EV")
 while state: state=redred(x="EV")
